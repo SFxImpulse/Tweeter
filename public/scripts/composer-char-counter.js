33bf401d28ffea01f8d94d6b75c3d481f2f7ef8e
SFxImpulse <braverman.david.business@gmail.com>
2022-07-19 20:30:19 +0000

    add dynamic counter, it changes colour if negative

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+  $('#tweet-text').on('input', function() {
+    let count = 140 - $(this).val().length;
+    let counter = $(this).siblings(".form-submission").children('.counter');
+    counter.val(count);
+    if (count < 0) {
+      counter.addClass("negative");
+    }
+    if (count > 0 && counter.hasClass("negative")) {
+      counter.removeClass("negative");
+    }
+  });
+});
